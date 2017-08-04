558bca6f473bad68b06f2de583ecef480501a59a
zhangzhi0903 <18310127735@163.com>
2017-08-04 16:51:32 +0800

    图片验证码

---
+++ ZGValidationView/Utils/ZGValidationView.h
@@ -0,0 +1,13 @@
+//
+//  ZGValidationView.h
+//  ZGValidationView
+//
+//  Created by offcn_zcz32036 on 2017/8/4.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZGValidationView : UIView
+
+@end
---
+++ ZGValidationView/Utils/ZGValidationView.m
@@ -0,0 +1,28 @@
+//
+//  ZGValidationView.m
+//  ZGValidationView
+//
+//  Created by offcn_zcz32036 on 2017/8/4.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import "ZGValidationView.h"
+
+@implementation ZGValidationView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self createUI];
+    }
+    return self;
+}
+#pragma mark - createViews
+-(void)createUI
+{
+    
+}
+
+
+@end
